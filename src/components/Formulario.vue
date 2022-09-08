eb68171bc9584daa5225d0a9d78ffb4056e688d8
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-09-08 00:44:56 -0300

    Cronometrando tarefas

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -17,15 +17,15 @@
           class="is-flex is-align-items-center is-justify-content-space-between"
         >
           <section>
-            <strong>00:00:00</strong>
+            <strong>{{ tempoDecorrido }}</strong>
           </section>
-          <button class="button">
+          <button class="button" @click="iniciar()">
             <span class="icon">
               <i class="fas fa-play"></i>
             </span>
             <span>play</span>
           </button>
-          <button class="button">
+          <button class="button" @click="finalizar()">
             <span class="icon">
               <i class="fas fa-stop"></i>
             </span>
@@ -43,5 +43,30 @@ import { defineComponent } from "vue";
 export default defineComponent({
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Formulario",
+  data() {
+    return {
+      tempoEmSegundos: 0,
+    };
+  },
+  computed: {
+    tempoDecorrido(): string {
+      return new Date(this.tempoEmSegundos * 1000)
+        .toISOString()
+        .substring(19, 11);
+    },
+  },
+  methods: {
+    iniciar() {
+      // Começa a contagem
+      // 1 segundo é igual a 1000 milesegundos
+      setInterval(() => {
+        this.tempoEmSegundos += 1;
+      }, 1000);
+      console.log("Iniciando a aplicação");
+    },
+    finalizar() {
+      console.log("Finalizando a aplicação");
+    },
+  },
 });
 </script>
